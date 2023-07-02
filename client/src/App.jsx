822a8ef4bd029464f4115ad0e8950c570037178a
Jackeline Castro <jackelinecastrocardoso@hotmail.com>
2023-07-02 11:24:16 -0300

    client e server to do list

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+import InputTodo from './components/InputTodo'
+import ListTodos from './components/ListTodos'
+
+function App() {
+  
+  return (
+    <>
+      <div className='container'>
+        <InputTodo/>
+        <ListTodos/>
+      </div>
+     
+      
+    </>
+  )
+}
+
+export default App
